70d0cd8c66f7a56ff9d3c9337ff3d14e8f6200b2
Naikaj <naikaj18@gmail.com>
2024-10-04 15:45:28 -0700

    Sign in updated

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -11,12 +11,11 @@ const FormField = ({
   otherStyles,
   ...props
 }) => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword,  setShowPassword] = useState(false);
 
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-
       <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
