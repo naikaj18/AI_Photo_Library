053ba89d4b38bc16996a6918a554d90f7a7e541a
Naikaj <naikaj18@gmail.com>
2024-10-06 22:27:41 -0700

    Appwrite logic II complete

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -4,14 +4,32 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants"
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link , router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
+import { Alert } from 'react-native'
 
 const SignIn = () => {
 const[form,setForm] = useState({email:'',password:''})
 
 const [isSubmitting,setIsSubmitting] = useState(false)
 
-const submit = () => {}
+const submit = async() => {
+  if (!form.password || !form.email){
+    Alert.alert("Error","Please fill in all the credentials")
+  }
+  setIsSubmitting(true);
+  try {
+    await signIn(form.email,form.password);
+
+    router.replace('/home')
+  } catch (error) {
+    Alert.alert("Error",error.message)
+  }
+  finally{
+    setIsSubmitting(false);
+  }
+  
+}
 
   return (
     <SafeAreaView className="bg-primary h-full">
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,66 +1,91 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from "../../constants"
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
-import { createUser } from '../../lib/appwrite'
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
+import { createUser } from "../../lib/appwrite";
+import { router } from "expo-router";
 
 const SignUp = () => {
-const[form,setForm] = useState({email:'',password:'', username:''})
+  const [form, setForm] = useState({ email: "", password: "", username: "" });
 
-const [isSubmitting,setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-const submit = () => {
-  createUser();
-}
+  const submit = async () => {
+    if (!form.username || !form.password || !form.email) {
+      Alert.alert("Error", "Please fill in all the credentials");
+    }
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="w-full justify-center min-h-[85vh] px-4 my-6">
-          <Image source={images.logo}
-          resizeMode='contain'
-          className="w-[115px] h-[35px]"/>
-          <Text className="text-white text-2xl font-psemibold mt-10">Sign up to Aora</Text>
-        
-            {/* For Username */}
-            <FormField
-              title ="Username"
-              value={form.username}
-              handleChangeText={(e)=>setForm({...form,username: e})}
-              otherStyles="mt-10"
-              />
-            {/* For email */}
-            <FormField
-              title ="Email"
-              value={form.email}
-              handleChangeText={(e)=>setForm({...form,email: e})}
-              otherStyles="mt-7"
-              keyboardType="email-address"/>
-            {/* For password */}
-            <FormField
-              title ="Password"
-              value={form.password}
-              handleChangeText={(e)=>setForm({...form,password: e})}
-              otherStyles="mt-7"
-              keyboardType="Password"/>
-            <CustomButton
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-white text-2xl font-psemibold mt-10">
+            Sign up to Aora
+          </Text>
+
+          {/* For Username */}
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+          {/* For email */}
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          {/* For password */}
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+            keyboardType="Password"
+          />
+          <CustomButton
             title="Sign Up"
             containerStyles="mt-8"
             handlePress={submit}
-            isLoading = {isSubmitting}
-            />
-        <View className="justify-center pt-5 flex-row gap-2">
-          <Text className="text-gray-100 font-pregular text-lg">Have an account already?</Text>
-          <Link href="/sign-in" className="text-lg text-gray-100 font-psemibold text-secondary-100">Sign In</Link>
-        </View>
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-gray-100 font-pregular text-lg">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg text-gray-100 font-psemibold text-secondary-100"
+            >
+              Sign In
+            </Link>
+          </View>
         </View>
       </ScrollView>
-
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
